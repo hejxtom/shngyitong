129aade999a43067d6c9e195959aed3bc42cce41
hejxtom <hejxtom@163.com>
2022-05-20 10:17:30 +0800

    尚医通前端项目初始化

--- front/yygh-parent/yygh-parent/common/common-util/src/main/java/com/atguigu/yygh/common/handler/GlobalExceptionHandler.java
+++ front/yygh-parent/yygh-parent/common/common-util/src/main/java/com/atguigu/yygh/common/handler/GlobalExceptionHandler.java
@@ -1,10 +1,38 @@
 package com.atguigu.yygh.common.handler;
+
+import com.atguigu.yygh.common.exception.YyghException;
+import com.atguigu.yygh.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @Classname GlobalExceptionHandler
  * @Description TODO
- * @Date  9:54 2022/5/20
+ * @Date 9:54 2022/5/20
  * @Created by hejx
  */
 
+@ControllerAdvice  //异常处理
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return  Result.fail();
+    }
+
+    /**
+     * 自定义全局异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(YyghException.class)
+    @ResponseBody
+    public Result error(YyghException e){
+        return Result.build(e.getCode(),e.getMessage());
+    }
+
+
 }
