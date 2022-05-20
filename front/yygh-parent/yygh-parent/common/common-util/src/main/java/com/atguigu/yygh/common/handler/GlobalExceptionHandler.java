00c0737f4744dd044cb43f019379e7798835a905
hejxtom <hejxtom@163.com>
2022-05-20 10:15:34 +0800

    尚医通前端项目

---
+++ front/yygh-parent/yygh-parent/common/common-util/src/main/java/com/atguigu/yygh/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.atguigu.yygh.common.handler;
+/**
+ * @Classname GlobalExceptionHandler
+ * @Description TODO
+ * @Date  9:54 2022/5/20
+ * @Created by hejx
+ */
+
+public class GlobalExceptionHandler {
+}
